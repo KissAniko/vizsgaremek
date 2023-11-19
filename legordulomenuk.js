92dfef043e0147d10b83a218987fb142be5e8f6c
KissAniko <126045521+KissAniko@users.noreply.github.com>
2023-11-19 13:18:50 +0100

    javítás

--- legordulomenuk.js
+++ legordulomenuk.js
@@ -2,10 +2,20 @@ let kalkulatorContent = document.getElementById("kalkulatorContent")
 let tableContent = document.getElementById("tableContent")
 
 function mouseEnteredCalculator() {
+	tableContent.style.display = "block"
+	kalkulatorContent.style.display="none"
+}
+
+function mouseEnteredCalculator2() {
 	kalkulatorContent.style.display = "block"
+	tableContent.style.display = "none"
 }
 
 function mouseLeavedCalculator() {
+	tableContent.style.display = "none"
+}
+
+function mouseLeavedCalculator2() {
 	kalkulatorContent.style.display = "none"
 }
 
